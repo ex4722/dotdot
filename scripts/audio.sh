c996750e66529a71f9c313d4d637ae19e7fb8a54
Eddie Xiao <thy.isnis@gmail.com>
2023-09-07 00:40:37 -0400

    Stuff

---
+++ scripts/audio.sh
@@ -0,0 +1,2 @@
+pactl set-default-sink "alsa_output.usb-Plantronics_Plantronics_Blackwire_5220_Series_46CA081846874D4B8B5FED55D03B96F4-00.analog-stereo"
+pactl set-default-sink "alsa_output.pci-0000_00_1f.3.analog-stereo"
