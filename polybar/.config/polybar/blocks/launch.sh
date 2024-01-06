d8b0737c62caed4b298cbc8798d0715428b0532c
Eddie Xiao <thy.isnis@gmail.com>
2024-01-05 19:24:20 -0500

    gitignore

--- polybar/.config/polybar/blocks/launch.sh
+++ polybar/.config/polybar/blocks/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/blocks"
+DIR="/opt/dotdot/polybar/.config/polybar/blocks"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -13,8 +13,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch the bar
 # polybar -q main -c "$DIR"/config.ini &
 
-style='blocks'
+
+echo $DIR/config.ini
 for m in $(polybar --list-monitors | cut -d":" -f1); do
         HEIGHT=33 FONT0=11 FONT0=22 MONITOR=$m polybar -q main -c "$DIR/config.ini" &	
 done
+# polybar -q main -c "$DIR/config.ini" &
 
