a251b11eb10f2146da84b6c1e066e6fad298c902
Eddie Xiao <thy.isnis@gmail.com>
2024-01-05 19:09:06 -0500

    diagflow

---
+++ nvim/.config/nvim/nvim/lua/ex/plugins/lsp/diagflow.lua
@@ -0,0 +1,9 @@
+return {
+    "dgagn/diagflow.nvim",
+    event = "LspAttach",
+    opts = {
+        placement = "inline",
+        inline_padding_left = 10,
+        toggle_event = { "InsertEnter" },
+    },
+}
