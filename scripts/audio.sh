62119527d090ce76d07626bc71b7887b0fdfad94
Eddie Xiao <thy.isnis@gmail.com>
2024-01-05 20:30:36 -0500

    ads scirpts stash

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -1,2 +1,5 @@
 pactl set-default-sink "alsa_output.usb-Plantronics_Plantronics_Blackwire_5220_Series_46CA081846874D4B8B5FED55D03B96F4-00.analog-stereo"
 pactl set-default-sink "alsa_output.pci-0000_00_1f.3.analog-stereo"
+
+bluetoothctl connect 84:D3:52:81:CC:8E
+pactl set-default-sink "bluez_output.84_D3_52_81_CC_8E.1"
