f321dff5073c63097a24a13308e29a8e8effd288
Eddie Xiao <thy.isnis@gmail.com>
2024-01-05 20:42:25 -0500

    Updates monitor scripts

--- scripts/monitor_off.sh
+++ scripts/monitor_off.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2560x240 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+if hostname | grep "nova"
+then
+    xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output VIRTUAL2 --off
+else
+    xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2560x240 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+fi
+
+i3-msg reload
+i3-msg restart
+
+nitrogen --restore
--- scripts/monitor_on.sh
+++ scripts/monitor_on.sh
@@ -2,6 +2,7 @@
 if hostname | grep "nova"
 then
     xrandr --output eDP1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP1 --off --output DP2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output VIRTUAL2 --off
+    echo nova
 else
     xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2560x240 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal
 fi
@@ -10,4 +11,3 @@ i3-msg reload
 i3-msg restart
 
 nitrogen --restore
-
