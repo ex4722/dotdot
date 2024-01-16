42a660adb8f3b5f918e5dbf0256f48b98dd3f8ff
Eddie Xiao <thy.isnis@gmail.com>
2024-01-16 14:42:57 -0500

    fuck clangd

--- nvim/.config/nvim/lua/ex/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/ex/plugins/lsp/lspconfig.lua
@@ -97,7 +97,7 @@ return {
             --     require("clangd_extensions.inlay_hints").setup_autocmd()
             --     require("clangd_extensions.inlay_hints").set_inlay_hints()
             -- end,
-            cmd = { "clangd-15", "--inlay-hints=true" },
+            cmd = { "clangd", "--inlay-hints=true" },
         })
 
         lspconfig.tsserver.setup({
