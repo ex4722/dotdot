382d2f36845526c1b0a86ef2dcc5d992ff249f79
Eddie Xiao <thy.isnis@gmail.com>
2024-02-07 15:38:54 -0500

    Updates lua_ls config

--- nvim/.config/nvim/lua/ex/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/ex/plugins/lsp/lspconfig.lua
@@ -39,61 +39,73 @@ return {
         --     on_attach = on_attach,
         -- })
 
+
+        require 'lspconfig'.lua_ls.setup {
+            on_init = function(client)
+                local path = client.workspace_folders[1].name
+                if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                        Lua = {
+                            runtime = {
+                                -- Tell the language server which version of Lua you're using
+                                -- (most likely LuaJIT in the case of Neovim)
+                                version = 'LuaJIT'
+                            },
+                            -- Make the server aware of Neovim runtime files
+                            workspace = {
+                                checkThirdParty = false,
+                                library = {
+                                    vim.env.VIMRUNTIME
+                                    -- "${3rd}/luv/library"
+                                    -- "${3rd}/busted/library",
+                                }
+                                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                                -- library = vim.api.nvim_get_runtime_file("", true)
+                            }
+                        }
+                    })
+
+                    client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                end
+                return true
+            end
+        }
+
         -- lspconfig.lua_ls.setup({
         --     capabilities = capabilities,
         --     on_attach = on_attach,
-        --     settings = { -- custom settings for lua
-        --         Lua = {
-        --             -- make the language server recognize "vim" global
-        --             diagnostics = {
-        --                 globals = { "vim" },
-        --             },
-        --             workspace = {
-        --                 -- make language server aware of runtime files
-        --                 library = {
-        --                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-        --                     [vim.fn.stdpath("config") .. "/lua"] = true,
+        --     on_init = function(client)
+        --         client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+        --             Lua = {
+        --                 runtime = {
+        --                     -- Tell the language server which version of Lua you're using
+        --                     -- (most likely LuaJIT in the case of Neovim)
+        --                     version = "LuaJIT",
+        --                 },
+        --
+        --                 diagnostics = {
+        --                     globals = { "vim", "use", "winid" },
+        --                 },
+        --                 -- Make the server aware of Neovim runtime files
+        --                 workspace = {
+        --                     checkThirdParty = false,
+        --                     library = {
+        --                         vim.env.VIMRUNTIME,
+        --                         [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        --                         [vim.fn.stdpath("config") .. "/lua"] = true,
+        --                         [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        --                     },
+        --                     -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        --                     -- library = vim.api.nvim_get_runtime_file("", true)
         --                 },
         --             },
-        --         },
-        --     },
-        -- })
+        --         })
         --
+        --         client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        --         return true
+        --     end,
+        -- })
 
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            on_init = function(client)
-                client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using
-                            -- (most likely LuaJIT in the case of Neovim)
-                            version = "LuaJIT",
-                        },
-
-                        diagnostics = {
-                            globals = { "vim", "use", "winid" },
-                        },
-                        -- Make the server aware of Neovim runtime files
-                        workspace = {
-                            checkThirdParty = false,
-                            library = {
-                                vim.env.VIMRUNTIME,
-                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                [vim.fn.stdpath("config") .. "/lua"] = true,
-                                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                            },
-                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                            -- library = vim.api.nvim_get_runtime_file("", true)
-                        },
-                    },
-                })
-
-                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-                return true
-            end,
-        })
         lspconfig.clangd.setup({
             capabilities = capabilities,
             on_attach = on_attach,
