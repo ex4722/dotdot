2cbb68dd9f48b119dce12ae53225e208617e796c
Eddie Xiao <thy.isnis@gmail.com>
2024-11-13 20:04:27 -0500

    Fuck telly

--- nvim/.config/nvim/lua/ex/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ex/plugins/telescope.lua
@@ -31,12 +31,5 @@ return {
         }
 
         telescope.load_extension("fzf")
-
-        -- Keymaps 
-        local keymap = vim.keymap
-        keymap.set("n", "<leader>pf","<cmd>Telescope find_files<CR>" , { desc = "Project find file" })
-        keymap.set("n", "<leader>pr", "<cmd>Telescope live_grep<CR>", { desc = "Project rip grep" })
-        keymap.set("n", "<leader>pg", "<cmd>Telescope grep_string<CR>", { desc = "Project grep word under string" })
-        keymap.set("n", "<C-x><C-b>", "<cmd>Telescope buffers<CR>", { desc = "Emacs buffer moment" })
     end
 }
