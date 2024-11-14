acbea539d8e339fea8d22703467bf6c0e4a7d44c
Eddie Xiao <thy.isnis@gmail.com>
2024-11-14 02:55:09 -0500

    Why not performant?

--- nvim/.config/nvim/lua/ex/plugins/fzf.lua
+++ nvim/.config/nvim/lua/ex/plugins/fzf.lua
@@ -7,7 +7,7 @@ return {
     require("fzf-lua").setup({})
     local keymap = vim.keymap
     keymap.set("n", "<leader>pf","<cmd>FzfLua files<CR>" , { desc = "Project find file" })
-    keymap.set("n", "<leader>pr", "<cmd>FzfLua live_grep<CR>", { desc = "Project rip grep" })
+    keymap.set("n", "<leader>pr", "<cmd>FzfLua live_grep_native<CR>", { desc = "Project rip grep" })
     keymap.set("n", "<C-x><C-b>", "<cmd>FzfLua buffers<CR>", { desc = "Emacs buffer moment" })
   end
 }
