edecd3bb2f2746cb7730bf3e79dc81b9b10333e3
Eddie Xiao <thy.isnis@gmail.com>
2024-11-13 20:03:48 -0500

    Adds fzf-lua

---
+++ nvim/.config/nvim/lua/ex/plugins/fzf.lua
@@ -0,0 +1,13 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons", { "junegunn/fzf", build = "./install --bin" } },
+  config = function()
+    -- calling `setup` is optional for customization
+    require("fzf-lua").setup({})
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>pf","<cmd>FzfLua files<CR>" , { desc = "Project find file" })
+    keymap.set("n", "<leader>pr", "<cmd>FzfLua live_grep<CR>", { desc = "Project rip grep" })
+    keymap.set("n", "<C-x><C-b>", "<cmd>FzfLua buffers<CR>", { desc = "Emacs buffer moment" })
+  end
+}
